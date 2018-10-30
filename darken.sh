0e18c2414e82cc5c1d628725e8ed1561919f4559
Matt Rixman <matt.rixman@clover.com>
2018-10-30 12:31:33 -0600

    picked suggestion colors

--- darken.sh
+++ darken.sh
@@ -7,9 +7,12 @@ export CONSOLE_THEME="dark"
 # sourced on startup
 echo "$this_file_parent_dir/darken.sh" > ~/.config/colorshift/target_file.txt
 
+# modify gnome-terminal default profile
 "$this_file_parent_dir/set_gnome_default" Dark
 
+# apply dircolors
 source "$this_file_parent_dir/dircolors.sh"
 dircolors_file ~/.config/dircolors/dircolors-solarized/dircolors.ansi-dark
 
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=3'
+# zsh suggestion style
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
--- lighten.sh
+++ lighten.sh
@@ -7,9 +7,12 @@ export CONSOLE_THEME="light"
 # sourced on startup
 echo "$this_file_parent_dir/lighten.sh" > ~/.config/colorshift/target_file.txt
 
+# modify gnome-terminal default profile
 "$this_file_parent_dir/set_gnome_default" Light
 
+# apply dircolors
 source "$this_file_parent_dir/dircolors.sh"
 dircolors_file ~/.config/dircolors/dircolors-solarized/dircolors.ansi-dark
 
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=3'
+# zsh suggestion style
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=180'
